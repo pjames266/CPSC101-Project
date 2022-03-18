b83336294b092375ad65bb12739734c9bf4d9e00
James Price <pjames266@gmail.com>
2022-03-18 00:10:50 -0700

    Added some World Stuff

--- Quest For Fire.py
+++ Quest For Fire.py
@@ -1,11 +1,11 @@
 import pygame
 import os
 
-
+pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 1500, 850
+WIDTH, HEIGHT = 1536, 832
 
 GAME_STATE = 1
 
@@ -13,16 +13,24 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Quest For Fire 1.0")
 FPS = 60
 
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
 
 BANNER = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'title_card.png')), (WIDTH, HEIGHT))
-GRASS = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'grass_texture_1.png')), (WIDTH, HEIGHT))
+GRASS = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'grass_texture_1.png')), (TILESIZE, TILESIZE))
 PLAYER_TEMP = pygame.image.load(os.path.join('Assets','sam.png'))
+
+
 
 class Sprite:
 
@@ -74,23 +82,30 @@ class Player(Entity):
         WIN.blit(PLAYER, (self.sprite.x,self.sprite.y))
 
 
-def draw_window_opening():
-    WIN.blit(BANNER, (0,0))
+class World:
 
-    pygame.display.update()
+    def __init__(self):
+        self.player = Player(500,500,TILESIZE,TILESIZE,100,6)
 
-def draw_window_game(steve):
-    WIN.blit(GRASS, (0,0))
+    def draw(self):
+        WIN.fill(DARKGREY)
+        self.draw_grid()
+        self.player.show()
+        pygame.display.update()
 
-    steve.show()
-    
-
-    pygame.display.update()
-
-
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(WIN, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(WIN, LIGHTGREY, (0, y), (WIDTH, y))
+        
+        for x in range(0, WIDTH, TILESIZE):
+            for y in range(0, HEIGHT, TILESIZE):
+                WIN.blit(GRASS, (x,y))
+        
 def main():
 
-    steve = Player(500,500,30,30,100,3)
+    world = World()
 
     clock = pygame.time.Clock()
     run = True
@@ -105,14 +120,9 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         
-        steve.player_movement(keys_pressed)
+        world.player.player_movement(keys_pressed)
+        world.draw()
 
-        if GAME_STATE == 0:
-            draw_window_opening()
-            
-
-        if GAME_STATE == 1:
-            draw_window_game(steve)
 
 if __name__== "__main__":
     main()
